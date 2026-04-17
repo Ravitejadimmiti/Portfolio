c43537ce1943073974db6ffa741ee54817e7deb0
raviteja.d <raviteja.d@abhibus.com>
2026-04-17 14:45:34 +0530

    New updates

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portfolio Loaded");
+window.addEventListener("scroll", () => {
+    document.querySelectorAll("section").forEach(section => {
+        let position = section.getBoundingClientRect().top;
+        let screenPosition = window.innerHeight / 1.2;
+
+        if (position < screenPosition) {
+            section.style.opacity = "1";
+            section.style.transform = "translateY(0)";
+        }
+    });
+});
